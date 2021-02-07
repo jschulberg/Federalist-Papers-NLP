3754b94c9d20c262857df3200398d6a80d868bc4
Justin Schulberg <schulbergjustin@gmail.com>
2021-02-07 13:13:07 -0500

    Wrote code to read all text files into one dataframe

--- EDA.py
+++ EDA.py
@@ -41,20 +41,31 @@ Counter(words).most_common(100)
 #%% Combining all files together 
 import io
 
+# Initialize an empty dataframe to hold all of our text
+text_df = pd.DataFrame(columns = ["Lines", "Essay"])
+
 # List all of the files in our Data folder
 files = os.listdir("Data")
 
-# Initialize an empty dataframe to hold all of our text
-text_df = pd.DataFrame()
+# TODO Loop through all text files and read them into singular dataframe
+for text_file in files:
+    
+    print(f"\n============================")
+    print(f"About to load {text_file} in")
+    print(f"============================")
+        
+    temp_df = pd.read_csv('Data/' + text_file, # Read the text file in our data folder
+                          delimiter = '\n', # Looks like every line ends with \n
+                          header = None, # Read from the first line
+                          names = ["Lines"], # Column header
+                          error_bad_lines = False) # Skip bad lines (there are like 6 across all essays)
+    
+    # Add the file name in so we have the essay number for reference
+    temp_df['Essay'] = text_file
+    
+    # Append the temporary dataframe with all corresponding text into our master dataframe
+    text_df = text_df.append(temp_df)
 
-lines = io.StringIO()   
+#%%% Take a look at our data
+text_df.head(10)
 
-for file_dir in files:
-    with open("Data/" + file_dir, 'r', encoding = "windows-1252") as file:
-        print(file.read())
-        lines.write(file.read())
-        lines.write('\n')
-        text_df.append(lines)
-
-lines.seek(0)       
-print(lines.read())
